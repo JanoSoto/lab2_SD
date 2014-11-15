87083c785118a34a185c41bbf63f53869ea71577
JanoSoto <alejandro.soto@usach.cl>
2014-11-15 16:27:58 -0300

    Falta eliminar - Cliente PHP

--- otros_clientes/PHP/cliente.php
+++ otros_clientes/PHP/cliente.php
@@ -6,20 +6,48 @@
 </head>
 <body>
 	<?php class Peticiones {
-		public function get(){
+		public function getAll(){
 			$homepage = file_get_contents('http://localhost:3000//beers.json');
 			$string = json_decode($homepage);
 			$count = count($string);
 			$cont = 0;
-		
+					
 			echo "<b>Hay un total de $count cervezas registradas:</b><br> <br>";
 			foreach($string as $clave=>$beer){
-				echo "La cerveza nº". ++$clave ." se llama $beer->name. Se describe como una cerveza '$beer->description' <br>";
+				echo "La cerveza nº". ++$clave ." se llama $beer->name. Se describe como una cerveza '$beer->description'   ";
+				echo "<a href=\"http://localhost:3000/beers/$beer->id\">Ver</a>  ";
+				echo "<a href=\"http://localhost:3000/beers/$beer->id/edit\">Editar</a>  ";
+				echo "<a data-confirm=\"¿Está seguro(a)?\" data-method=\"DELETE\" href=\"http://localhost:3000/beers/$beer->id\" >Eliminar</a><br>";	
+
+				//echo "<br><br><a href=\"localhost/otros_clientes/PHP/cliente.php\">Volver</a>";
 			}
 		}
 		
+
 		public function post(){
-			echo "<h1>FUNCION POST</h1>";
+			echo "<h1>Crear una nueva cerveza - Cliente PHP</h1>";
+			echo "<form action=\"http://localhost:3000/beers\" method=\"post\">";
+			echo "<label>Nombre: </label> <input id=\"beer_name\" name= \"beer[name]\" type=\"text\"><br>";
+			echo "<label>Descripción: </label> <input id=\"beer_description\" name=\"beer[description]\"type=\"text\"><br>";
+			echo "<input type=\"submit\" value=\"Enviar\"><br>";
+			echo "</form>";
+
+			echo "<br><br><a href=\"localhost/otros_clientes/PHP/cliente.php\">Volver</a>";
+		}
+
+		public function put(){
+			echo "<h1>Crear una nueva cerveza - Cliente PHP</h1>";
+			echo "<form action=\"http://localhost:3000/beers\" method=\"put\">";
+			echo "<label>Nombre: </label> <input id=\"beer_name\" name= \"beer[name]\" type=\"text\"><br>";
+			echo "<label>Descripción: </label> <input id=\"beer_description\" name=\"beer[description]\"type=\"text\"><br>";
+			echo "<input type=\"submit\" value=\"Enviar\"><br>";
+			echo "</form>";	
+
+			echo "<br><br><a href=\"localhost/otros_clientes/PHP/cliente.php\">Volver</a>";
+		}
+
+		public function delete(){
+
 		}
 	}
 		
@@ -27,7 +55,7 @@
 	
 	<?php
 		$peticiones = new Peticiones;
-		$peticiones->post();
+		$peticiones->getAll();
 	?>
 
 </body>
